1629429a8b5e4672ad8db0bd73884ed3192c1c8b
keigomichi <hundredacrewood1165g7@gmail.com>
2022-07-25 00:56:43 +0900

    chore:

--- abc/abc261/d/main.py
+++ abc/abc261/d/main.py
@@ -1,19 +1,16 @@
 n, m = map(int, input().split())
-x = list(map(int, input().split()))
-c = [0] * m
-y = [0] * m
-for i in range(m):
-    c[i], y[i] = map(int, input().split())
-dp = [0] * (n + 1)
-cnt = 0
-for i in range(n):
-    bns = 0
-    for j in range(m):
-        if c[j] == cnt + 1:
-            bns = y[j]
-    if dp[i] > dp[i] + x[i] + bns:
-        dp[i + 1] = dp[i]
-    else:
-        dp[i + 1] = dp[i] + x[i] + bns
-        cnt += 1
-print(max(dp))
+x = [0] + list(map(int, input().split()))
+y = [0] * (n + 1)
+for _ in range(m):
+    c, v = map(int, input().split())
+    y[c] = v
+dp = [[-10000000000] * (n + 1) for _ in range(n + 1)]
+dp[0][0] = 0
+for i in range(1, n + 1):
+    for j in range(i + 1):
+        if j == 0:
+            dp[i][0] = dp[i - 1][0]
+        else:
+            dp[i][j] = dp[i - 1][j - 1] + x[i] + y[j]
+            dp[i][0] = max(dp[i][0], dp[i - 1][j])
+print(max(dp[n]))
---
+++ main.py
@@ -0,0 +1,5 @@
+n = 3
+l = [[0] * (n + 1) for _ in range(n + 1)]
+m = [[0] * (n + 1)] * (n + 1)
+print(f'l: {l}')
+print(f'm: {m}')
